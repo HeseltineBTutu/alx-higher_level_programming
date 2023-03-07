85e32f73549bf2dc6ae47933d1155c4687d28e02
HeseltineBTutu <heseltinetutu@gmail.com>
2023-03-07 05:11:43 -0800

    task 6 complete

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for num in range(10):
+    for j in range(num+1, 10):
+        if num == 0 and j == 1:
+            print("01", end=", ")
+        else:
+            print("{0:02d}".format(10*num+j), end=", ")
+print("{0:02d}".format((10*9) + 9))
