90c6ed8b0003f8a4c9e8da0410c5cc86d7710aef
HeseltineBTutu <heseltinetutu@gmail.com>
2023-03-08 00:47:44 -0800

    fix

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-for num in range(10):
-    for j in range(num+1, 10):
-        if num == 0 and j == 1:
-            print("01", end=", ")
+for digit1 in range(0, 10):
+    for digit2 in range(digit1+1, 10):
+        if digit1 == 8 and digit2 == 9:
+            print("{}{}".format(digit1, digit2))
         else:
-            print("{0:02d}".format(10*num+j), end=", ")
-print("{0:02d}".format(89))
+            print("{}{}".format(digit1, digit2), end=", ")
