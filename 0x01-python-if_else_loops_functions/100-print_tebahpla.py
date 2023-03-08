c6acce847de031003a1dd65fe8ee7fb3bb1156c0
HeseltineBTutu <heseltinetutu@gmail.com>
2023-03-08 04:50:59 -0800

    task 14 complete

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 for letter in range(90, 64, -1):
-    print("{0}{1}".format(chr(letter + 32) if letter % 2 == 0 else chr(letter), ""), end="")
+    if letter % 2 == 0:
+        letter = chr(letter + 32)
+    else:
+        letter = chr(letter)
+    print("{0}".format(letter), end="")
