c563b4d42a7e044a614d4852c2aa640817ef1506
HeseltineBTutu <heseltinetutu@gmail.com>
2023-03-08 05:13:37 -0800

    task 15 complete

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    """
+    This function creates a copy of the input string, removing the character
+    at the position n.
+
+    Parameters:
+        str (str): the input string
+        n (int): the position of the character to remove (0-based index)
+
+    Returns:
+        str: a copy of the input string with the chatacter at position n removed
+    """
+    return str[:n] + str[n+1:]
