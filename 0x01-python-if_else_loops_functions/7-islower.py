fb6b1e4a6f0f66e6e460025cc1a8af3108c0a43e
HeseltineBTutu <heseltinetutu@gmail.com>
2023-03-08 01:19:42 -0800

    task 7 complete

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -2,6 +2,6 @@
 def islower(c):
     """Function that checks for lowercase character"""
     if ord('a') <= ord(c) <= ord('z'):
-            return True
+        return True
     else:
         return False
