6330c5c43eab3b6668a824d364374c3fd63686f0
HeseltineBTutu <heseltinetutu@gmail.com>
2023-03-08 03:41:03 -0800

    task 12 complete

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    """Function that prints the numbers from 1 to 100 separated by a space."""
+    for number in range(1, 100):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz", end="")
+        elif number % 3 == 0:
+            print("Fizz", end="")
+        elif number % 5 == 0:
+            print("Buzz", end="")
+        else:
+            print(number, end="")
+        print(end=" ")
