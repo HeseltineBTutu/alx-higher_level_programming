52d8697a0df06d381fe0fe2112fbb7be53ee3656
HeseltineBTutu <heseltinetutu@gmail.com>
2023-03-09 05:12:28 -0800

    task 8 complete

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-import magic_calculation_102
-
-add = magic_calculation_102.add
-sub = magic_calculation_102.sub
+from magic_calculation_102 import add, sub
 
 def magic_calculation(a, b):
     if a < b:
