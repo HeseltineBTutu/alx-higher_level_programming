fcf9de854a9c052f1bdda426cb73878721222c76
HeseltineBTutu <heseltinetutu@gmail.com>
2023-12-04 03:12:44 -0800

    added a program that prints the number of and the list of its arguments.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,11 +5,13 @@ if __name__ == "__main__":
     arguments = sys.argv[1:]  # Exclude the script name from arguments
 
     num_args = len(arguments)
-    plural = 's' if num_args != 1 else ''
-    end_char = '.' if num_args == 0 else ':'
-    
-    print(f"Number of argument{plural}: {num_args}{end_char}")
 
-    for i, arg in enumerate(arguments, start=1):
-        print(f"{i}: {arg}")
+    if num_args == 0:
+        print("0 arguments.")
+    else:
+        plural = 's' if num_args != 1 else ''
+        print(f"{num_args} argument{plural}:")
+
+        for i, arg in enumerate(arguments, start=1):
+            print(f"{i}: {arg}")
 
