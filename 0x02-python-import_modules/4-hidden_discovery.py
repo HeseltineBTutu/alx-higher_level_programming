b49d21a10481fb6bdd594b617f634e2169e46b3d
HeseltineBTutu <heseltinetutu@gmail.com>
2023-12-04 04:06:47 -0800

    added a program that prints all the names defined by the compiled module hidden_4.pyc

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,8 @@
 
 if __name__ == "__main__":
     names = ['__init__', 'my_secret_santa', 'print_hidden', 'print_school']
-    filtered_names = sorted(name for name in names if not name.startswith('__'))
+    filtered_names = sorted(
+            name for name in names if not name.startswith('__')
+            )
     for name in filtered_names:
         print(name)
