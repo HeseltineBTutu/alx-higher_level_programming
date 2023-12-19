dc9f38e3bfecd6ffd7d05efae404ef0c60faa92b
HeseltineBTutu <heseltinetutu@gmail.com>
2023-12-19 08:46:54 -0800

    Refactor safe_print_list_integers function
    Updated safe_print_list_integers function to ensure it prints the first x integers from a list while handling cases where x might exceed the list's length. Added logic to track the count of printed integers and break the loop when x integers have been printed or when the list ends. This ensures correct behavior in handling x and the available elements in my_list.

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,13 +2,17 @@
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
     try:
-        for i in my_list:
-            if isinstance(i, int):
-                print("{:d}".format(i), end="")
-                count += 1
-                if count == x:
-                    break
-    except IndexError:
+        for element in my_list:
+            if count < x:
+                try:
+                    value = int(element)
+                    print("{:d}".format(value), end="")
+                    count += 1
+                except ValueError:
+                    pass
+            else:
+                break
+    except TypeError:
         pass
     print()
     return count
