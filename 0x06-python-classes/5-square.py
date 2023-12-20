0f435813310350f3f1b85ff1116db1ba1debff2d
HeseltineBTutu <heseltinetutu@gmail.com>
2023-12-20 10:15:51 -0800

    Add my_print method to Square class for printing squares with '#' characters

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+This module contains functionalities related to geometric shapes.
+"""
+
+
+class Square:
+    """
+    A class representing a square.
+
+    Attributes:
+    - size (int): The size of the square
+    """
+
+    def __init__(self, size=0):
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Calculate the area of the square."""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Print the square with '#' character."""
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
