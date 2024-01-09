7b0875a20bceb9922a5d91936c3e2f31790c23ef
HeseltineBTutu <heseltinetutu@gmail.com>
2024-01-09 09:47:29 -0800

    added  function that reads a text file and prints it out

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module reads from a file
+"""
+
+
+def read_file(filename=""):
+    """
+    A function  that reads a file.
+    
+    Attributes:
+    - filename - the name of the file
+    """
+    with open(filename, encoding="utf-8") as f:
+        read_data = f.read()
+    print(read_data, end="")
