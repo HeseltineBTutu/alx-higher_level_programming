c83435ff6db19a8f605d861e851d443fae42362a
HeseltineBTutu <heseltinetutu@gmail.com>
2024-01-10 07:19:31 -0800

    added public method def reload_from_json(self, json): that replaces all attributes of the Student instance

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+A class that defines a student
+"""
+
+
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            # If attrs is None or not provided, retrieve all attributes
+            return self.__dict__
+
+        filtered_attrs = {}
+        for attr in attrs:
+            if hasattr(self, attr):
+                filtered_attrs[attr] = getattr(self, attr)
+
+        return filtered_attrs
+
+    def reload_from_json(self, json):
+        for  key, value in json.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
