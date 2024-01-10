20833c30055eca7c433e53392149fe3f022bdf3a
HeseltineBTutu <heseltinetutu@gmail.com>
2024-01-10 05:29:30 -0800

    added a script that adds all arguments to a Python list, and then save them to a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,22 +3,20 @@
 A module adds all arguments to a Python list
 """
 import sys
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
 
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-# Getting the command-line arguments excluding the script name
-arguments = sys.argv[1:]
+    arguments = sys.argv[1:]
 
-try:
-    # Load existing data from file if available
-    existing_data = load_from_json_file('add_item.json')
-except FileNotFoundError:
-    # If the file does't exist, start with an empty list
-    existing_data = []
-
-# Add new arguments to the existing list
-existing_data.extend(arguments)
-
-# Save the updated list to add_item.json
-save_to_json_file(existing_data, 'add_item.json')
+    try:
+        existing_data = load_from_json_file('add_item.json')
+    except FileNotFoundError:
+        # If the file does't exist, start with an empty list
+        existing_data = []
+        # Add new arguments to the existing list
+    existing_data.extend(arguments)
+    # Save the updated list to add_item.json
+    save_to_json_file(existing_data, 'add_item.json')
