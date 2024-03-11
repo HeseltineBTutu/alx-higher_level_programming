6368126a33f3f218a9c95a722ece13d89f53137c
HeseltineBTutu <heseltinetutu@gmail.com>
2024-03-11 06:05:57 -0700

    fix

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,7 +2,6 @@
 
 const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let i = 0; i < languages.length; i++) {
-  const currentChar = languages[i];
-  console.log(currentChar);
+for (const language of languages) {
+  console.log(language);
 }
