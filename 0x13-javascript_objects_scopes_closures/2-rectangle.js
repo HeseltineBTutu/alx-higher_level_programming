4da6beb8707628d4577f75d16ad770e1fc3a7a76
HeseltineBTutu <heseltinetutu@gmail.com>
2024-03-13 00:26:07 -0700

    fix

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,8 +2,8 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w === 0 || h === 0 || w < 0 || h < 0) {
-      return;
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      return {};
     }
     this.width = w;
     this.height = h;
