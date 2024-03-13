0eb89a38a7e811758ac367ee30b7ff0864b4e190
HeseltineBTutu <heseltinetutu@gmail.com>
2024-03-13 02:09:14 -0700

    add task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+  }
+
+  print () {
+    const row = 'X'.repeat(this.width);
+    for (let i = 0; i < this.height; i++) {
+      console.log(row);
+    }
+  }
+};
