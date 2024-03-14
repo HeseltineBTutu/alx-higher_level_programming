f576512224cd652c27966860bd7c3bbff8787d55
HeseltineBTutu <heseltinetutu@gmail.com>
2024-03-14 08:50:05 -0700

    added task 9

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,8 @@
+exports.logMe = function (item) {
+  if (!exports.logMe.count) {
+    exports.logMe.count = 0; // Initialize count if it doesn't exist
+  }
+
+  console.log(`${exports.logMe.count}: ${item}`);
+  exports.logMe.count++;
+};
