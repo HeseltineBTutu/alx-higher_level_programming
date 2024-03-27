23458ced8cd889581dc66733fea64490b235ec9c
HeseltineBTutu <heseltinetutu@gmail.com>
2024-03-27 02:23:11 -0700

    fix

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a GET request to a URL and displays the body of the response
-curl -s -X GET "$1" -o /tmp/body_response && grep -Pzo '(?<=<body>).*?(?=</body>)' /tmp/body_response
+curl -sL "$1"
