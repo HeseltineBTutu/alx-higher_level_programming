ec08e24231dbfdb55143c4c78936f6bd1951d04b
HeseltineBTutu <heseltinetutu@gmail.com>
2024-03-27 02:15:28 -0700

    fix

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 # This script gets a URL and displays response body for 200 status codes 
 response=$(curl -s -w '%{http_code}' -o /dev/null $1); [[ $response == 200 ]] && curl -s $1
